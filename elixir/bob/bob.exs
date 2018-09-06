7e47a06a25d307d3df67feae8119c9d128c89e37
alice <rainingilove@gmail.com>
2018-09-06 10:57:19 +0200

    refactor bob.exs

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,19 +1,28 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.replace(input, " ", "") == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") ->
-        if String.upcase(input) == input and String.match?(input, ~r/[a-zA-Z]/)
-        do
-          "Calm down, I know what I'm doing!"
-        else
-          "Sure."
-        end
-      not String.match?(input, ~r/[a-zA-Z]/)
-      and String.match?(input, ~r/[0-9]/) -> "Whatever."
-      String.match?(input, ~r/[0-9%^*@#$(*^]/) -> "Whoa, chill out!"
-      String.upcase(input) == input -> "Whoa, chill out!"
+      is_sentence_empty(input) -> "Fine. Be that way!"
+      is_question(input) and is_sentence_upcase(input) ->
+        "Calm down, I know what I'm doing!"
+      is_question(input) -> "Sure."
+      is_sentence_upcase(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
+  end
+
+  def is_sentence_empty(input) do
+    String.replace(input, " ", "") == ""
+  end
+
+  def is_question(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp is_word_in(input) do
+    String.match?(input, ~r/[a-zA-ZА-Яа-я]/)
+  end
+
+  def is_sentence_upcase(input) do
+    is_word_in(input) and String.upcase(input) == input
   end
 end
