ef7e8a5285adf14d6ef2301a89c0a07140db474b
alice <rainingilove@gmail.com>
2018-09-11 10:25:13 +0200

    refactor rna transcription with map

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -7,20 +7,14 @@ defmodule RNATranscription do
   iex> RNATranscription.to_rna('ACTG')
   'UGAC'
   """
+  @rna_complements %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    dna
-    |> Enum.map(&to_rna_single/1)
-    |> Enum.join("")
-    |> String.to_charlist
-  end
-
-  defp to_rna_single(dna) do
-    case [dna] do
-      'G' -> "C"
-      'C' -> "G"
-      'T' -> "A"
-      'A' -> "U"
-    end
+    dna |> Enum.map(&@rna_complements[&1])
   end
 end
