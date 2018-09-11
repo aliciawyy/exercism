ce180cd0d4b70f57d2837efaf9193cd2c616cbf8
alice <rainingilove@gmail.com>
2018-09-11 17:31:06 +0200

    add roman numerals

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -2,22 +2,27 @@ defmodule Roman do
   @doc """
   Convert the number to a roman number.
   """
+  @count_to_roman %{
+    1 => {"I", "V", "X"},
+    10 => {"X", "L", "C"},
+    100 => {"C", "D", "M"},
+    1000 => {"M", "", ""}
+  }
+
   @spec numerals(pos_integer) :: String.t()
-  def numerals(number) when number < 10 do
-    1..number
-    |> Enum.map(fn _ -> "I" end)
-    |> Enum.join("")
-    |> String.replace("IIIIIIIII", "IX")
-    |> String.replace("IIIII", "V")
-    |> String.replace("IIII", "IV")
+  def numerals(number), do: numerals(number, 1000)
+
+  defp numerals(number, 1), do: digit_to_roman(number, 1)
+  defp numerals(number, count) do
+    digit_to_roman(div(number, count), count) <>
+    numerals(rem(number, count), div(count, 10))
   end
-  def numerals(number) when number < 100 do
-    roman = 1..div(number, 10)
-    |> Enum.map(fn _ -> "X" end)
-    |> Enum.join("")
-    |> String.replace(String.duplicate("X", 9) , "XC")
-    |> String.replace(String.duplicate("X", 5), "L")
-    |> String.replace(String.duplicate("X", 4), "XL")
-    roman <> numerals(rem(number, 10))
+
+  defp digit_to_roman(number, count) do
+    {r_one, r_five, r_ten} = @count_to_roman[count]
+    String.duplicate(r_one, number)
+    |> String.replace(String.duplicate(r_one, 9), r_one <> r_ten)
+    |> String.replace(String.duplicate(r_one, 5), r_five)
+    |> String.replace(String.duplicate(r_one, 4), r_one <> r_five)
   end
 end
